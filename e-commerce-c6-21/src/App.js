c03b9dd31dbefb63e2263d0a7e5b67bc0b7ff19c
Thomas Kurz <thomaskurz.15@gmail.com>
2022-08-14 12:56:36 -0300

    movi counter a la carpeta counter

--- e-commerce-c6-21/src/App.js
+++ e-commerce-c6-21/src/App.js
@@ -1,7 +1,7 @@
 import react from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Cart from './components/Cart/Cart';
 //components
 
 
