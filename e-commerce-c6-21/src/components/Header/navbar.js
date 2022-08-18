0a4f40d58f9d7403f1f84d2a30023e44f3a1225f
Thomas Kurz <thomaskurz.15@gmail.com>
2022-08-17 21:18:12 -0300

    updating navbar

---
+++ e-commerce-c6-21/src/components/Header/navbar.js
@@ -0,0 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Outlet, Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMountain } from '@fortawesome/free-solid-svg-icons';
+import './navbar.css'
+
+function NavbarComponent() {
+  return (
+    <>
+      <Navbar bg='dark' variant = 'dark' sticky='top' expand='lg' collapseOnSelect>
+          <Navbar.Brand className='logo-brand'>          
+              <Nav.Link href='/'><FontAwesomeIcon icon={faMountain} className='icon-hat'></FontAwesomeIcon>Andes</Nav.Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href='vista1'>vista1</Nav.Link>
+              <Nav.Link href='vista2'>vista2</Nav.Link>
+              <Nav.Link href='vista3'>vista3</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+          
+      </Navbar>
+      <Outlet />
+
+    </>
+  );
+}
+
+export default NavbarComponent;
