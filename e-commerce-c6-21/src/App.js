4f096ff4d16ac53421b7421f3ffe21f72d24ed38
Thomas Kurz <thomaskurz.15@gmail.com>
2022-08-17 21:39:14 -0300

    added route to cart

--- e-commerce-c6-21/src/App.js
+++ e-commerce-c6-21/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
 
 //components
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<NavbarComponent />}>
           <Route index element={<Home />} />
+          <Route path="/cart" element={<Cart />}></Route>
           {/* <Route path="vista1" element={<'vista1' />} />
           <Route path="vista2" element={<'vista2' />} />
           <Route path="vista3" element={<'vista3' />} /> */}
