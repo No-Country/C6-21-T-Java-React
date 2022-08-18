6004c5961524c926c6d48e1fe8d79d4a00184031
Thomas Kurz <thomaskurz.15@gmail.com>
2022-08-17 21:21:14 -0300

    updating app

--- e-commerce-c6-21/src/App.js
+++ e-commerce-c6-21/src/App.js
@@ -1,26 +1,24 @@
-import react from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Cart from './components/Cart/Cart';
-//components
-import Header from './components/Header/Header';
-import ProductDetailContainer from './components/ProductDetailContainer/ProductDetailContainer';
-import ProductsListContainer from './components/ProductsListContainer/ProductsListContainer';
+import NavbarComponent from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
-//views
-import Home from './views/Home/Home';
-import CartItem from './components/Cart/CartItem';
+//components
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route exact path="/" element={ <Home/> }/>
-        <Route exact path="/products" element={ <ProductsListContainer/> } />
-        <Route exact path="/product/:productId" element={<ProductDetailContainer />} />
-        <Route exact path="/cart" element={ <Cart/> } />
+        <Route path="/" element={<NavbarComponent />}>
+          <Route index element={<Home />} />
+          {/* <Route path="vista1" element={<'vista1' />} />
+          <Route path="vista2" element={<'vista2' />} />
+          <Route path="vista3" element={<'vista3' />} /> */}
+        </Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
