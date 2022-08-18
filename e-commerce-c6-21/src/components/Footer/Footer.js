abd9ca253e5c19de313360c9a4e4ac71945fa818
Thomas Kurz <thomaskurz.15@gmail.com>
2022-08-17 21:19:43 -0300

    updating footer

--- e-commerce-c6-21/src/components/Footer/Footer.js
+++ e-commerce-c6-21/src/components/Footer/Footer.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMountain } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faSlack } from '@fortawesome/free-brands-svg-icons';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="footer__col1">
+        <div className='hat-icon-footer'><FontAwesomeIcon icon={faMountain}></FontAwesomeIcon></div>
+        <div className='description-footer'>
+          <h2>Andes</h2>
+          <p>Proyecto cohorte 6 - No Country</p>
+        </div>
+      </div>
+      <div className="footer__col3">
+        <h4 style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>@No Country</h4>
+        <div className='contact__icons-2 d-flex justify-content-evenly'>
+          <a className='link' href='https://github.com/No-Country' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+          <a className='link' href='https://linkedin.com/company/nocountryforjuniordevs/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+          <a className='link' href='https://no-country.slack.com/archives/C02KW0N1BS9' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faSlack}></FontAwesomeIcon></a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
