a9c9604b64eb22856e635564fa7fba498f865c51
Thomas <Thomaskurz.15@gmail.com>
2022-08-22 19:29:35 -0300

    added context to cart item

--- e-commerce-c6-21/src/components/Cart/CartItem.js
+++ e-commerce-c6-21/src/components/Cart/CartItem.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
-
+import { useCartContext } from '../../context/CartContext'
 import ItemCount from '../Counter/itemCount';
 
 
 
 function CartItem({imgUrl, id, title, price, qty, totalPrice, stock}) {
 
-     //Hay qe implementar context, y crear las funciones para borrar item y modificarlo
-    function deleteItem() {
-        
-    }
-
-    function cartModification() {
-        
-    }
-
+    const {deleteItem, cartModification} = useCartContext();
     //las funciones de arriba tienen que salir del context y no ser declaradas aca. solo las puse para que no se rompa 
     
     function deleteI () {
