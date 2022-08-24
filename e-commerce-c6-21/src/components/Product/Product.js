fdc7c8a95d8205d08515b215649e016ebc78cd4b
Felipe Maldonado <felipe.arica@gmail.com>
2022-08-23 23:55:50 -0400

    testing products

--- e-commerce-c6-21/src/components/Product/Product.js
+++ e-commerce-c6-21/src/components/Product/Product.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const product_data = [
+  {id: 1, name:"remera negra", description: 'Remera de algodón negra', image:"../media/poleraNegra.png", price:300},
+  {id: 2, name:"remera blanca", description: 'Remera de algodón blanca', image:"./media/poleraBlanca.jpeg", price:200},
+  {id: 3, name:"remera azul", description: 'Remera de algodón azul con cuello', image:"./media/poleraAzul.png", price:350},
+  {id: 4, name:"remera naranja", description: 'Remera de algodón naranja', image:"./media/poleraNaranja.jpeg", price:150},
+  {id: 5, name:"remera deportiva blanca", description: 'Remera deportiva dry fit', image:"./media/poleraDeportiva.jpeg", price:400},]
+
 const Product = () => {
+  console.log(product_data)
   return (
-    <div>Product</div>
+    <div>
+      <h1>Hola</h1>
+    </div>
   )
 }
 
--- e-commerce-c6-21/src/components/home/Home.js
+++ e-commerce-c6-21/src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './home.css';
+import Product from '../Product/Product';
 
 const Home = () => {
   return (
     <div className='home'>
       <div className='home__title'>
         <h1>Bienvenido</h1>
+        <Product />
       </div>
         </div>
 
