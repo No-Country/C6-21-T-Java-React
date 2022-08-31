bf32c5f88cff935612cb381f3944e7961472e9e8
Felipe Maldonado <felipe.arica@gmail.com>
2022-08-31 04:48:21 -0400

    structure of products card done

--- e-commerce-c6-21/src/components/Product/Product.js
+++ e-commerce-c6-21/src/components/Product/Product.js
@@ -1,25 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import styled from '@emotion/styled';
+import Button from 'react-bootstrap/Button';
+import './product.css';
+
+const Imagen = styled.img`
+  width: 12rem;
+`;
 
 const Product = () => {
 
-  const [loading2, setloading2] = useState(true)
+  const [loading3, setloading3] = useState(true)
 
   const [products, listedProducts] = useState([])
 
   useEffect(()=>{
-    setloading2(true)
+    setloading3(true)
     const db = getFirestore()
     const productCollection = collection(db, 'productos')
     
     getDocs(productCollection)
     .then (resp => listedProducts(resp.docs.map( item => ({id: item.id, ...item.data()}))))
     .catch((err)=>console.log(err))
-    .finally(setloading2(false))
+    .finally(setloading3(false))
 },[])
 console.log(products)
   return (
-    <div>Product</div>
+    <section>
+      {(loading3) ?
+
+      <h1>Loading</h1>
+      :
+      products.map((item, index) => (
+    <li>
+      <div>
+        <div>
+          <Imagen src={item.image} alt='imagen'/>
+        </div>
+        <div>
+          <h1>{item.name}</h1>
+          <p>{item.description}</p>
+        </div>
+        <div>
+          <div>{item.price}</div><div><Button variant="light">Añadir al carro</Button>{' '}</div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </li>
+      ))
+    }
+    </section>
   )
 }
 
